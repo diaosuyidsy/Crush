2d6b05db4a04db6965eef0d4ac1941c985a33803
Suyi Diao <suyi.diao@vanderbilt.edu>
2017-09-07 00:38:50 -0500

    updated:

--- Assets/Script/Slow_Block.cs
+++ Assets/Script/Slow_Block.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Slow_Block : MonoBehaviour {
+public class Slow_Block : MonoBehaviour
+{
 
-	[Range(0.0f, 5.0f)]
+	[Range (0.0f, 5.0f)]
 	public float Slow_Ratio;
 
-	void OnCollisionEnter2D(Collision2D other){
-		if(other.gameObject.tag == "Bullet"){
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		if (other.gameObject.tag == "Bullet") {
 			Vector2 v2 = other.gameObject.GetComponent<Rigidbody2D> ().velocity;
 			v2 = new Vector2 (v2.x * Slow_Ratio, v2.y * Slow_Ratio);
 			other.gameObject.GetComponent<Rigidbody2D> ().velocity = v2;
-			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Bullet") {
+			Vector2 v2 = other.gameObject.GetComponent<Rigidbody2D> ().velocity;
+			v2 = new Vector2 (v2.x * Slow_Ratio, v2.y * Slow_Ratio);
+			other.gameObject.GetComponent<Rigidbody2D> ().velocity = v2;
 		}
 	}
 }
